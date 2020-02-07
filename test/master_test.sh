36a31cec3425b9b1e649bbe611655fa6b8870cba
CodeCanna <fleabass4@gmail.com>
2020-02-06 23:14:22 -0700

    Added comments and prettied code.

--- test/master_test.sh
+++ test/master_test.sh
@@ -6,20 +6,20 @@ set -o posix;
 source ./redis_connection_test.sh;
 source ./get_and_set_test.sh;
 
+# Run the Redis connection test
 test_redis_connection;
 if [ "$?" != 0 ]; then
-    echo "master_test: FAILED!";
+    echo "master_test: FAILED!";  # Echo Failed results
     exit 1;
 fi
 
+# Run the set and get test
 test_set_and_get;
 if [ "$?" != 0 ]; then
-    echo "master_test: FAILED!";
+    echo "master_test: FAILED!";  # Echo Failed results
     exit 1;
 fi
 
+# Echo passing results
 echo "master_test: PASSED!";
-
-sleep 2;
-
 echo "All Tests Passed!";
