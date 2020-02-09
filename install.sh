6e7bc8e75564a1a80c003fd3e99e869010ba86be
CodeCanna <fleabass4@gmail.com>
2020-02-08 23:46:19 -0700

    Changed installer dialogue to --verbose and took away my custom messages.

--- install.sh
+++ install.sh
@@ -14,64 +14,59 @@ exp_dir=/home/$SUDO_USER/Documents/Work/Notes;
 
 # Define install directory
 install_dir=/bin;
-    
+
 # Define GenReport library directory
 lib_dir=/lib/GenReport;
 
 # Define help_doc directory
 help_dir=/usr/share;
 
+# GenReport Installer
 function install_genreport() {
     echo "Starting install";
-
+    
     # Create lib directory if it does NOT exist
     if [ ! -d "$lib_dir" ]; then
-        echo "Creating $lib_dir";
         sleep 0.5;
         
-        if (! mkdir --parents "$lib_dir") then
+        if (! mkdir --verbose --parents "$lib_dir") then
             echo "Couldn't create $lib_dir";
             exit 1;
         fi
     fi
     
     if [ ! -d "$exp_dir" ]; then
-        echo "Creating directory $exp_dir";
         sleep 0.5;
         
-        if (! mkdir --parents "$exp_dir" && chown "$SUDO_USER" "$exp_dir") then
+        if (! mkdir --verbose --parents "$exp_dir" && chown "$SUDO_USER" "$exp_dir") then
             echo "Problem creating directory $exp_dir";
         fi
     fi
-
+    
     # Install GenReport Library
-    echo "Installing GenReport/lib";
     sleep 0.5;
-    if (! cp -r ./lib/* $lib_dir) then
+    if (! cp --recursive --force --verbose ./lib/* $lib_dir) then
         echo "There was a problem copying ./lib to $lib_dir";
         exit 1;
     fi
-
+    
     # Install test library
-    echo "Installing GenReport/test";
     sleep 0.5
-    if(! cp -r ./test "$lib_dir") then
+    if(! cp --recursive --force --verbose ./test "$lib_dir") then
         echo "There was a problem installing the test library...";
         exit 1;
     fi
-
+    
     # Install help doc
-    echo "Installing GenReport/help_doc";
     sleep 0.5;
-    if (! cp ./help_doc.txt "$help_dir") then
+    if (! cp --force --verbose ./help_doc.txt "$help_dir") then
         echo "There was a problem copying ./help_doc.txt to $help_dir";
         exit 1;
     fi
     
     # Install GenReport
-    echo "Installing GenReport";
     sleep 0.5;
-    if (! cp ./gen-report "$install_dir") then
+    if (! cp --recursive --force --verbose ./gen-report "$install_dir") then
         echo "There was a problem copying ./gen-report to $install_dir";
         exit 1;
     fi
@@ -79,35 +74,40 @@ function install_genreport() {
     echo "Installation Sucessfull!!";
 }
 
-function uninstall_genreport() {    
+# GenReport Uninstaller
+function uninstall_genreport() {
+    # Check if GenReport is even installed
+    if [ ! -f /bin/gen-report ]; then
+        echo "GenReport not found, Nothing to do...";
+        exit 1;
+    fi
+    
     # Remove export directory
-    echo "Removing $exp_dir";
     sleep 0.5;
-    if (! rm -rf "$exp_dir") then
+    if (! rm --recursive --force --recursive --force --verbose "$exp_dir") then
         echo "There was a problem removing $exp_dir";
         exit 1;
     fi
     
-    # Remove library
-    echo "Removing $lib_dir";
+    # Remove libraries
     sleep 0.5;
-    if (! rm -rf "$lib_dir") then
+    if (! rm --recursive --force --recursive --force --verbose "$lib_dir") then
         echo "There was a problem removing $lib_dir/GenReport";
         exit 1;
     fi
     
+    # Remove test library
+    
     # Remove gen-report
-    echo "Uninstalling GenReport";
     sleep 0.5;
-    if (! rm -f "$install_dir/gen-report") then
+    if (! rm --force --verbose "$install_dir/gen-report") then
         echo "There was a problem removing $install_dir/gen-report";
         exit 1;
     fi
-
+    
     # Remove help_doc
-    echo "Removing $help_dir/help_doc.txt";
     sleep 0.5;
-    if (! rm -f "$help_dir"/help_doc.txt) then
+    if (! rm --force --verbose "$help_dir"/help_doc.txt) then
         echo "Couldn't remove $help_dir/help_doc.txt";
         exit 1;
     fi
